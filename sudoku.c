c45c9bbd03052026fdf3a7229377e6b70ae3c173
Your Name <you@example.com>
2024-05-16 16:12:21 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -7,12 +7,14 @@ typedef struct{
    int sudo[9][9];
 }Node;
 
-Node* createNode(){
+Node* createNode()
+{
   Node* n=(Node*) malloc(sizeof(Node));
   return n;
 }
 
-Node* copy(Node* n){
+Node* copy(Node* n)
+{
     Node* new=(Node*) malloc(sizeof(Node));
     *new = *n;
     return new;
@@ -48,9 +50,37 @@ int is_valid(Node* n){
     return 1;
 }
 
+/*
+1.Cree una función que a partir de un nodo genere una lista con los nodos adyacentes:
 
-List* get_adj_nodes(Node* n){
-    List* list=createList();
+Recuerde que los nodos adyacentes son generados aplicando las acciones al estado actual.
+
+Para el caso del ejemplo, la función debería retornar una lista con 9 nodos. Cada uno de ellos idéntico al nodo original pero cambiando el valor de la primera casilla vacía, es decir: sudo[0][2], por 1, 2, ..., 9.
+Utilice la función Node* copy(Node* n) para copiar nodos
+*/
+
+List* get_adj_nodes(Node* n)
+{
+    List* list = createList();
+    int i, j;
+   //recorro la matriz
+   for (i = 0; i < 9; i++)
+   {
+      for(j = 0; j < 9; j++)
+      {
+         if(n->sudo[i][j] == 0)
+         {
+            for(int k = 1; k < 10; k++)
+            {
+               Node *aux = copy(n);
+               aux->sudo[i][j] = k;
+               pushBack(list, aux);
+               
+            }
+         }
+      }
+   }
+   
     return list;
 }
 
